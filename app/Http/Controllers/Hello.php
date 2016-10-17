df895a99ec3d3514b5b6b92f9e9705aa1cf9e3d2
17100113 <s.hashimhaider@gmail.com>
2016-10-17 18:01:39 +0500

    - First commit, eva

---
+++ app/Http/Controllers/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class Hello extends Controller
+{
+    public function index() {
+        print_r('hello group 13');
+    }
+}
