47add5e4ade4d2b56460eb4575bc1298bacf2d98
17100113 <s.hashimhaider@gmail.com>
2016-10-17 19:05:03 +0500

    - DB test

--- app/Http/Controllers/Hello.php
+++ app/Http/Controllers/Hello.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class Hello extends Controller
 {
-    public function index() {
-        print_r('hello group 13');
+    public function index($param) {
+        $users = DB::table('users')->get();
+        var_dump($users);
     }
 }
