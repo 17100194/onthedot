e71415e6ede31bd0b21a9d98e80b991a85f92aa2
17100113 <s.hashimhaider@gmail.com>
2017-04-06 20:33:43 +0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => $database,
             'prefix' => '',
         ],
 
@@ -72,11 +72,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
