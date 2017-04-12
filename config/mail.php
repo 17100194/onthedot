043d0d4630aa5884d9501d0f5033bfe476377ecf
17100113 <s.hashimhaider@gmail.com>
2017-04-12 20:57:39 +0500

    -

--- config/mail.php
+++ config/mail.php
@@ -2,14 +2,15 @@
 
 return array(
     "driver" => "smtp",
-    "host" => "smtp.mailtrap.io",
-    "port" => 2525,
+    "host" => "smtp.gmail.com",
+    "port" => 587,
     "from" => array(
-        "address" => "833dc2bd47-135297@inbox.mailtrap.io",
-        "name" => "Example"
+        "address" => "onthedotpk@gmail.com",
+        "name" => "OntheDot"
     ),
-    "username" => "fcd3632add1901",
-    "password" => "13feae878ead79",
+    'encryption' => 'tls',
+    "username" => "onthedotpk@gmail.com",
+    "password" => "Google453",
     "sendmail" => "/usr/sbin/sendmail -bs",
     "pretend" => false
 );
