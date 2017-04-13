3e2840d76fd8a0e2893093407d77bc099e3281df
17100113 <s.hashimhaider@gmail.com>
2017-04-13 11:46:15 +0500

    -

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -152,6 +152,7 @@ class HomeController extends Controller
             $courseData = $app->make('stdClass');
             $courseData->type = 'course';
             $courseData->meetingid = "";
+            $courseData->with = "";
             $courseData->name = $course->name;
             $courseData->timing = $course->timing;
             $courseData->section = $course->section;
