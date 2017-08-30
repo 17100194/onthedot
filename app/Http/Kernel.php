f1308e49c090c4316b035774384302b4f0d0a9b4
17100113 <s.hashimhaider@gmail.com>
2017-08-30 13:59:41 +0500

    - Almost there!

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        HttpsProtocol::class,
     ];
 
     /**
@@ -32,6 +31,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            HttpsProtocol::class,
         ],
 
         'api' => [
